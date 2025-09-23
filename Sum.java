839479ab1fb1da811593a2836b31cce381b18bcd
SaurabhSaraswat2112 <saraswats2112002@gmail.com>
2025-09-23 12:41:57 +0530

    Sum of N natural numbers

---
+++ Sum.java
@@ -0,0 +1,14 @@
+public class Sum {
+    static int sum(int n){
+        if(n == 0){
+            return 0;
+        }
+        return n+sum(n-1);
+    }
+    public static void main(String[] args){
+        int num = 5;
+        int res = sum(num);
+        System.out.println("Sum of first "+num+" natural numbers is: "+res);
+    }
+    
+}
