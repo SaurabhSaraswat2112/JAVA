839564f3eed8610c19223cbf8bc501051ab30840
SaurabhSaraswat2112 <saraswats2112002@gmail.com>
2025-09-23 21:05:23 +0530

    Swap Two Numbers

---
+++ SwapNum1.java
@@ -0,0 +1,14 @@
+public class SwapNum1 {
+    public static void main(String[] args){
+        int a = 5;
+        int b = 10;
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+
+        // Swapping without using a temporary variable
+        a = a+b;
+        b = a-b;
+        a = a-b;
+
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
